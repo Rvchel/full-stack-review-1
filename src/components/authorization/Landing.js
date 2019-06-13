dbe4d1e86022a2e4641a1ba4b15ad700f5acce29
Sandra Rodgers <SRODG7@GMAIL.COM>
2019-06-13 10:52:31 -0500

    routes setup

---
+++ src/components/authorization/Landing.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+
+class Landing extends Component {
+    constructor(){
+        super()
+            this.state={}
+        
+    }
+
+    render(){
+        return(
+            <div>Landing</div>
+        )
+    }
+
+}
+export default Landing
---
+++ src/components/authorization/Registration.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+
+class Registration extends Component {
+    constructor(){
+        super()
+            this.state={}
+        
+    }
+
+    render(){
+        return(
+            <div>Registration</div>
+        )
+    }
+
+}
+export default Registration
