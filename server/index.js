60b66595e72142e97a165630e4092475d1f70888
Sandra Rodgers <SRODG7@GMAIL.COM>
2019-06-13 14:11:05 -0500

    registration working

--- server/index.js
+++ server/index.js
@@ -2,15 +2,18 @@ require("dotenv").config();
 const express = require("express");
 const massive = require("massive");
 const session = require("express-session");
+const {registerUser, loginUser, getUser, logout} = require('./controllers/authController')
 
 const app = express();
+
+
 let { SERVER_PORT, SESSION_SECRET } = process.env;
 
 app.use(express.json());
 
 app.use(
   session({
-    secret: SESSION_SECRET,
+    secret: 'dog',
     resave: false,
     saveUninitialized: true,
     cookie: {
@@ -18,6 +21,16 @@ app.use(
     }
   })
 );
+
+
+
+//endpoint
+app.post('/auth/register', registerUser)
+app.post('/auth/login', loginUser)
+app.get('/auth/user', getUser)
+app.post('/auth/logout', logout)
+
+
 
 massive(process.env.CONNECTION_STRING).then(db => {
   app.set("db", db);
--- src/components/authorization/Landing.js
+++ src/components/authorization/Landing.js
@@ -1,18 +1,24 @@
-import React, {Component} from 'react';
-
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class Landing extends Component {
-    constructor(){
-        super()
-            this.state={}
-        
-    }
+  constructor() {
+    super();
+    this.state = {};
+  }
 
-    render(){
-        return(
-            <div>Landing</div>
-        )
-    }
-
+  render() {
+    return (
+      <div>
+        <Link to="/login">
+          <button>Login</button>
+        </Link>
+        <Link to="/register">
+          <button>Create Account</button>
+        </Link>
+        <button>Logout</button>
+      </div>
+    );
+  }
 }
-export default Landing
+export default Landing;
